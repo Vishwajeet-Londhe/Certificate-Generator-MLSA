4d6e6611165cabb30c06ce4ba9dd666daf032e6b
VishwajeetLondhe <vishwajeetlondhe51@gmail.com>
2023-11-13 16:44:41 +0530

    initial commit

---
+++ main_certificate.py
@@ -0,0 +1,99 @@
+
+import os
+
+#os.system("pip install -r requirements.txt")
+
+import csv
+from certificate import *
+from docx import Document
+from docx2pdf import convert
+from openpyxl import Workbook, load_workbook
+
+mailerpath = "Data/Mail.xlsm"
+htmltemplatepath = "Data/mailtemplate.html"
+
+# create output folder if not exist
+try:
+    os.makedirs("Output/Doc")
+    os.makedirs("Output/PDF")
+except OSError:
+    pass
+
+
+def updatemailer(row, workbook, sheet, email, filepath, sub, body, status, cc=""):
+    sheet.cell(row=row, column=1).value = email
+    sheet.cell(row=row, column=2).value = cc
+    sheet.cell(row=row, column=3).value = sub
+    sheet.cell(row=row, column=4).value = body
+    sheet.cell(row=row, column=5).value = filepath
+    sheet.cell(row=row, column=6).value = status
+    workbook.save(filename = mailerpath)
+
+def getworkbook(filename):
+    wb = load_workbook(filename=filename, read_only=False, keep_vba=True)
+    sheet = wb.active
+    return wb, sheet
+
+def gethtmltemplate(htmltemplatepath=htmltemplatepath):
+    return open(htmltemplatepath, "r").read()
+
+def getmail(name, event, ambassador):
+    sub = f"[MLSA] Certificate of Participation for {name}"
+    html = gethtmltemplate(htmltemplatepath)
+    body = html.format(name=name, event=event, ambassador=ambassador)
+    return sub, body
+
+def get_participants(f):
+    data = [] # create empty list
+    with open(f, mode="r", encoding='iso-8859-1') as file:
+        csv_reader = csv.DictReader(file)
+        for row in csv_reader:
+            data.append(row)# append all results
+    return data
+
+def create_docx_files(filename, list_participate):
+
+    wb, sheet = getworkbook(mailerpath)
+
+    event = input("Enter the event name: ")
+    ambassador = input("Enter Ambassador Name: ")
+
+    for index, participate in enumerate(list_participate):
+        # use original file everytime
+        doc = Document(filename)
+
+        name = participate["Name"]
+        email = participate["Email"]
+
+        replace_participant_name(doc, name)
+        replace_event_name(doc, event)
+        replace_ambassador_name(doc, ambassador)
+
+        doc.save('Output/Doc/{}.docx'.format(name))
+
+        doc.save('Output/Doc/{}.docx'.format(name))
+
+        # ! if your program working slowly, comment this two line and open other 2 line.
+        print("Output/{}.pdf Creating".format(name))
+        convert('Output/Doc/{}.docx'.format(name), 'Output/Pdf/{}.pdf'.format(name))
+
+        filepath = os.path.abspath('Output/Pdf/{}.pdf'.format(name))
+
+        sub, body = getmail(name, event, ambassador)
+
+        updatemailer(row=index+2, workbook=wb,  sheet=sheet, email=email, filepath=filepath, sub=sub, body=body, status="Send")
+
+    
+# get certificate temple path
+certificate_file = "Data/Event Certificate Template.docx"
+# get participants path
+participate_file = "Data/"+("Participant.csv" if (input("Test Mode (Y/N): ").lower())[0]=="n" else "temp.csv")
+
+# get participants
+list_participate = get_participants(participate_file);
+
+# process data
+create_docx_files(certificate_file, list_participate)
+
+
+
---
+++ readme.md
@@ -0,0 +1,49 @@
+# Microsoft Learn Student Ambassador Certificate Automation
+
+### Check out this video below:
+
+[![IMAGE ALT TEXT](http://img.youtube.com/vi/OUAbqdLDTZQ/0.jpg)](http://www.youtube.com/watch?v=OUAbqdLDTZQ "How to use Certificate Generator MLSA")
+
+This repo simply use a template certificate docx file and generates certificates
+both docx and pdf.
+
+###  Working on Windows only.
+
+## Run these commands in your terminal
+
+```
+git clone https://github.com/Sabyasachi-Seal/Certificate-Generator-MLSA
+cd Certificate-Generator-MLSA
+```
+Now Copy your Participant List to the Data Folder and rename it as `ParticipantList.csv`. <br>
+<e><i>The list must have the following fields: ```Name, Email```</i></e>. It may have more, but these 2 are essential.
+```
+pip install -r requirements.txt
+python main_certificate.py
+```
+
+## *Important*
+### Do not use this button to run your code:
+![Screenshot from 2023-08-26 10-36-02](https://github.com/Sabyasachi-Seal/Certificate-Generator-MLSA/assets/36451386/6e4ddf15-c97a-4c1a-9e64-9cd4db416511)
+
+### Use ```python main_certificate.py``` to run your code
+
+
+## Customization
+- You can change the certificate template file in the `Data` folder.
+- You can change the email template file in the `Data` folder.
+
+## How to send emails?
+- You can use the `Mail.xlsm` file to send emails to the participants. Open this with Excel. Press ```Allow Content``` if required.
+- Do not need to change anything in the file itself.
+- All you need to do is to search for ```View Macros```  in excel and then select the ```Send_Mails``` macro and then click on ```Run```.
+- Now open outlook and login.
+- Click on outbox and see the mails being sent one by one.
+
+## Further Releases (Planning)
+- Implementing for Linux users?
+
+<h2></h2>
+
+
+Souce Repo(I made some subtle improvements) : <a href="https://github.com/muhammedogz/MLSA-Certificate-Automate">Click Here</a>
